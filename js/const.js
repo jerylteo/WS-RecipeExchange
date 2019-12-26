773145ad0bc12af17ab1dca42428980557b562af
Jeryl <52659891+jerylteo@users.noreply.github.com>
2019-12-26 11:20:52 +0800

    master

--- js/const.js
+++ js/const.js
@@ -1,3 +1,6 @@
 const BASE_URL = "http://localhost/lrv_RecipeExchange/api/";
 const GET_RECIPES_URL = BASE_URL + "recipes";
-const RECIPES_ID_URL = BASE_URL + "recipes/";
+const GET_RECIPES_ID_URL = BASE_URL + "recipes/";
+const PUT_RECIPE_URL = BASE_URL + "recipes/";
+const ADD_RECIPE_URL = BASE_URL + "add_recipe";
+const DEL_RECIPE_ID_URL = BASE_URL + "recipes/";
--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 new Vue({
     el: '#app',
     data: {
+        recipe: {
+            name: '',
+            category: '',
+            description: '',
+            author: '',
+        },
         recipes: null,
+        editStatus: false,
     },
     methods: {
         initialise() {
@@ -11,23 +18,75 @@ new Vue({
             axios.get(GET_RECIPES_URL)
             .then(response=> {
                 this.recipes = response.data;
+                console.log(this.recipes);
             })
             .catch(error=>{
                 console.log(error);
             })
         },
-        getRecipe(id) {
-            axios.get(GET_RECIPES_ID_URL+id)
+        getRecipe(param) {
+            axios.get(param)
             .then(response=>{
                 this.recipes = response.data;
                 console.log(this.recipes);
             })
             .catch(error=>{
-                this.recipes = response.data;
+                console.log(error);
             })
         },
         edit(id) {
+            this.getRecipe(`${GET_RECIPES_ID_URL}${id}`);
+            this.editStatus = true;
+        },
+        update(id) {
+            var form = new FormData();
+            form.append('name', this.recipe.name);
+            form.append('description', this.recipe.description);
+            form.append('category_id', this.recipe.category);
+            form.append('author', this.recipe.author);
 
+            axios.post(`${PUT_RECIPE_URL}${id}`, form)
+            .then(response=>{
+                console.log(response.data);
+                alert("Update Successful");
+                this.editStatus = false;
+                this.initialise();
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        },
+        addRecipe() {
+            var form = new FormData();
+            form.append('name', this.recipe.name);
+            form.append('description', this.recipe.description);
+            form.append('category_id', this.recipe.category);
+            form.append('author', this.recipe.author);
+
+            axios.post(`${ADD_RECIPE_URL}`, form)
+            .then(response=>{
+                console.log(response.data);
+                alert("Added Successfully");
+                this.initialise();
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        },
+        removeRecipe(id) {
+            var cfm = confirm("Confirm delete?");
+
+            if (cfm) {
+                axios.delete(`${DEL_RECIPE_ID_URL}${id}`)
+                .then(response=>{
+                    console.log(response.data);
+                    alert("Delete Successful");
+                    this.initialise();
+                })
+                .catch(error=>{
+                    console.log(error);
+                })
+            }
         }
     },
     mounted() {
